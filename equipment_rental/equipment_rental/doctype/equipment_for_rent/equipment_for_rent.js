07c63abdc0588ee1a2fa72449bd2c5ef3ee2cd5a
John Park <yuntan0@gmail.com>
2023-03-08 10:01:57 +0000

    example for education

---
+++ equipment_rental/equipment_rental/doctype/equipment_for_rent/equipment_for_rent.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, John and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Equipment For Rent', {
+	// refresh: function(frm) {
+
+	// }
+});
